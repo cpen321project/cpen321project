838d0d61d56de8fc66df16f0cdd6f0a5754bba5e
i951 <rebeccatslee@gmail.com>
2022-07-06 12:21:33 -0700

    remove unnecessary comments

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,12 @@ const app = express()
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
-// global.io = io
 
-const Message = require('./models/Message.js')
-const chatEngine = require('./controllers/chatEngine.js')
-
-const chatEngineRouter = require('./routes/chatEngine-routes.js')
 const port = "3010"
 
 const userStore = require('./controllers/userStore.js')
 const courseManager = require('./controllers/courseManager.js')
+const chatEngine = require('./controllers/chatEngine.js')
 
 app.use(express.json())
 
@@ -40,157 +36,26 @@ app.get('/', (req, res) => {
     res.send('Server is running on port: ' + port)
 })
 //---------------------------------------------------------------------------------------
-/*app.get("/getuserprofile/:userID", (req, res) => {
-    userCollection.find({userID: req.params.userID}).toArray((err, userProfileResult) => {
-      if (err) {
-        console.error(err)
-        res.status(500).json({ err: err })
-        return
-      }
-      res.status(200).json(userProfileResult)
-    })
-})*/
-
+// routes for userStore
 app.get("/getuserprofile/:userID", userStore.getUserProfile)
-
-
-/*app.get("/getstudentlist/:coursename", (req, res) => {
-    var coursenamespace = req.params.coursename.substring(0,4)+ " "+req.params.coursename.substring(4,req.params.coursename.length)
-    dbCourse.collection(coursenamespace).find({}).project({userID:1, displayName:1, _id:0}).toArray((err, resultstudent) => {
-      if (err) {
-        console.error(err)
-        res.status(500).json({ err: err })
-        return
-      }
-      res.status(200).json(resultstudent)
-    })
-})*/
-
-app.get("/getstudentlist/:coursename", courseManager.getStudentList)
-
-/*app.get("/getcourselist/:userID", (req, res) => {
-    userCollection.find({userID: req.params.userID}).project({courselist:1, _id:0}).toArray((err, resultcourse) => {
-      if (err) {
-        console.error(err)
-        res.status(500).json({ err: err })
-        return
-      }
-      res.status(200).json(resultcourse)
-    })
-})*/
-
 app.get("/getcourselist/:userID", userStore.getCourseList)
-
-
-/*app.post("/addusertocourse", async (req, res) => {
-    try{
-        await dbCourse.collection(req.body.coursename).insertOne({
-            displayName: req.body.displayName,
-            userID: req.body.userID,
-          })
-        res.status(200).send("User added successfully\n")
-    }
-    catch(err){
-        console.log(err)
-        res.send(400).send(err)
-    }
-})*/
-
-app.post("/addusertocourse", courseManager.addUserToCourse)
-
-/*app.post("/addcoursetouser", async (req, res) => {
-    userCollection.updateOne({"userID": req.body.userID}, {$push:{"courselist":req.body.coursename}},(err, result)=>{
-        if (err) {
-            console.error(err)
-            res.status(500).json({ err: err })
-            return
-        }
-        res.status(200).json({ ok: true })
-    });
-})*/
-
-app.post("/addcoursetouser", courseManager.addCourseToUser)
-
-/*app.post("/createprofile", (req, res) => {
-    var courselistarr = [];
-    var blockeruserarr = [];
-  userCollection.insertOne(
-    {
-      displayName: req.body.displayName,
-      userID: req.body.userID,
-      coopStatus: req.body.coopStatus,
-      yearStanding: req.body.yearStanding,
-      courselist: courselistarr,
-      blockedUser: blockeruserarr
-    },
-    (err, result) => {
-      if (err) {
-        console.error(err)
-        res.status(500).json({ err: err })
-        return
-      }
-      res.status(200).json({ ok: true })
-    }
-  )
-})*/
-
 app.post("/createprofile", userStore.createProfile)
-
-/*app.post("/block", (req, res) => {
-    userCollection.updateOne({"userID": req.body.userID}, {$push:{"blockedUser":req.body.blockedUserAdd}},(err, result)=>{
-        if (err) {
-            console.error(err)
-            res.status(500).json({ err: err })
-            return
-        }
-        res.status(200).json({ ok: true })
-    });
-})*/
-
 app.post("/block", userStore.block)
 
-/*app.delete("/deleteuserfromcourse/:userID/:coursename", async (req, res) => {
-    try{
-      var coursenamespace = req.params.coursename.substring(0,4)+ " "+req.params.coursename.substring(4,req.params.coursename.length)
-        await 
-        dbCourse.collection(coursenamespace).deleteOne({"userID": req.params.userID})
-        res.status(200).send("User deleted successfully\n")
-    }
-    catch(err){
-        console.log(err)
-        res.status(400).send(err)
-    }
-})*/
-
+// routes for courseManager
+app.get("/getstudentlist/:coursename", courseManager.getStudentList)
+app.post("/addusertocourse", courseManager.addUserToCourse)
+app.post("/addcoursetouser", courseManager.addCourseToUser)
 app.delete("/deleteuserfromcourse/:userID/:coursename", courseManager.deleteUserFromCourse)
-
-/*app.post("/deletecoursefromuser", async (req, res) => {
-    try{
-        await 
-        userCollection.updateMany({"userID": req.body.userID},{$pull: {"courselist": req.body.coursename}})
-        res.status(200).send("Course deleted successfully\n")
-    }
-    catch(err){
-        console.log(err)
-        res.status(400).send(err)
-    }
-})*/
-
 app.post("/deletecoursefromuser", courseManager.deleteCourseFromUser)
-//---------------------------------------------------------------------------------------
-
-// todo: separate routes for each module 
-// localhost:3031/chat/getConversationByGroupID/:groupID
-// localhost:3031/chat/getPrivateConversationByUserNames/:senderName/:receiverName
-// app.use('/chat', chatEngineRouter)
 
 // routes for chatEngine
 app.get('/getConversationByGroupID/:groupID', chatEngine.getConversationByGroupID)
 app.get('/getPrivateConversationByUserNames/:senderName/:receiverName', chatEngine.getPrivateConversationByUserNames)
 
-var usersSockets = {}
+let usersSockets = {}
 
-// socketio connection
+// socketio connection - for real time sending and receiving messages
 io.on('connection', (socket) => {
     console.log('a user connected')
 
