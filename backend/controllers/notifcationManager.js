c5543c83a2369b1ecdedb36b446109bae2d6dc3f
i951 <rebeccatslee@gmail.com>
2022-07-16 22:30:20 -0700

    fix all issues in notifcationManager
    unnecessary blocks and object shorthand

--- backend/controllers/notifcationManager.js
+++ backend/controllers/notifcationManager.js
@@ -17,60 +17,46 @@ userCollection = dbUser.collection("userCollection")
 module.exports = {
     // function to initiate the notification for when a user is added to a course
     _userAddedNotification: async (userID, courseID) => {
+        // trying to get the the users in a course
+        let students = await dbCourse.collection(courseID).find({}).project({ userID: 1, displayName: 1, _id: 0 }).toArray();
 
-        try {
-            // trying to get the the users in a course
-            let students = await dbCourse.collection(courseID).find({}).project({ userID: 1, displayName: 1, _id: 0 }).toArray();
+        var take, regToken;
+        let otherstudents = students.filter(data => data.userID != userID);
+        theTokens = [];
+        otherstudents.forEach( async student => {
+            console.log("Student: "+ student.displayName)
+            try {
+                take = await userCollection.findOne({ userID: student.userID });
+                console.log("Take= "+ take.displayName)
+                regToken = take.registrationToken;
+                console.log("regToken: "+ regToken);
+            } catch (err) {
+                console.log("For finding user in userCollection : " + err);
+            }
+            
+            const message = {
+                notification: {
+                    title: 'A New User Joined ' + courseID,
+                    body: 'Say Hi to the new user who just joined the course ' + courseID,
+                },
+                token: regToken,
+            };
 
-            var take, regToken;
-            let otherstudents = students.filter(data => data.userID != userID);
-            theTokens = [];
-            otherstudents.forEach( async student => {
-                console.log("Student: "+ student.displayName)
-                try{
-                    take = await userCollection.findOne({ userID: student.userID });
-                    console.log("Take= "+ take.displayName)
-                    regToken = take.registrationToken;
-                    console.log("regToken: "+ regToken);
-                }catch(err){
-                    console.log("For finding user in userCollection : " + err);
-                }
-                
-                const message = {
-                    notification: {
-                        title: 'A New User Joined ' + courseID,
-                        body: 'Say Hi to the new user who just joined the course ' + courseID,
-                    },
-                    token: regToken,
-                };
-
-                admin.messaging().send(message)
-                    .then((response) => {
-                        if (response.failureCount > 0) {
-                            const failedTokens = [];
-                            response.responses.forEach((resp, idx) => {
-                                if (!resp.success) {
-                                    failedTokens.push(registrationTokens[idx]);
-                                }
-                            });
-                            console.log('List of tokens that caused failures: ' + failedTokens);
-                        }
-                        else {
-                            console.log('Successfully sent message to a user : ' + student.userID);
-                        }
-                    });
-            });
-
-        }
-
-        catch (err) {
-            console.log("For sending user added notification, the error:" + err);
-            // res.status(400).send(err)
-        }
-
-
-
-
+            admin.messaging().send(message)
+                .then((response) => {
+                    if (response.failureCount > 0) {
+                        const failedTokens = [];
+                        response.responses.forEach((resp, idx) => {
+                            if (!resp.success) {
+                                failedTokens.push(registrationTokens[idx]);
+                            }
+                        });
+                        console.log('List of tokens that caused failures: ' + failedTokens);
+                    } else {
+                        console.log('Successfully sent message to a user : ' + student.userID);
+                    }
+                });
+        });
     },
     get userAddedNotification() {
         return this._userAddedNotification;
@@ -79,94 +65,78 @@ module.exports = {
         this._userAddedNotification = value;
     },
 
-    newRegistrationToken : async (req,res) => {
-       try{
-        await userCollection.updateOne({userID: req.body.userID}, {$set: {registrationToken: req.body.registrationToken}})
-            if (err) {
-                console.error(err)
-                res.status(500).json({ err: err })
-                return
-            }
-            res.status(200).json({ok:true})
-            // resultstudent.forEach(student => {
-            //     if (student.userID != userID) {
-            //         userAddedNotification(student.userID, courseID)
-            //     }
-            // }
-            // )
-        }
-        catch (err) {
-            console.log("Could not update token for" + req.body.userID + " with the error " + err)
-            // res.status(400).send(err)
-        }
-       
+    newRegistrationToken: async (req,res) => {
+        await userCollection.updateOne({userID: req.body.userID}, {$set: {registrationToken: req.body.registrationToken}}, 
+            (err, result) => {
+                if (err) {
+                    console.log("Could not update token for: " + req.body.userID + ", with the error: " + err)
+                    res.status(400).send(err)
+                } else {
+                    console.error("newRegistrationToken successful")
+                    res.status(200).json({ok:true})
+                }
+            })
+        
+        // resultstudent.forEach(student => {
+        //     if (student.userID != userID) {
+        //         userAddedNotification(student.userID, courseID)
+        //     }
+        // }
+        // )
     },
 // displayName:0, userID:1, coopStatus:0, yearStanding:0, registrationToken:1, courselist:0, blockedUser:0
 
-    privateMessageNotification : async (senderName, receiverID) => {
+    privateMessageNotification: async (senderName, receiverID) => {
+        let resultstudent = await userCollection.findOne({ userID: receiverID }) 
 
-        try {
+        const message = {
+            notification: { 
+                title: 'Private Message from ' + senderName, 
+                body: "You've got a private message from " + senderName, 
+            },
+            token: resultstudent.registrationToken,
+        };
 
-            let resultstudent = await userCollection.findOne({ userID: receiverID }) 
+
+        admin.messaging().send(message)
+        .then((response) => {
+            if (response.failureCount > 0) {
+                const failedTokens = [];
+                response.responses.forEach((resp, idx) => {
+                    if (!resp.success) {
+                        failedTokens.push(registrationTokens[idx]);
+                    }
+                });
+                console.log('Private message notification failure, token: ' + failedTokens);
+            } else {
+                console.log('Successfully sent message notification to a user : ' + resultstudent.userID);
+            }
+        }); 
+    },
+
+    groupMessageNotification: async (senderName, groupID) => {
+        var take, userToken
+        let resultstudents = await dbCourse.collection(groupID).find({}).project({ userID: 1, displayName: 1, _id: 0 }).toArray(); 
         
+        resultstudents.forEach( async (student) => {
+            try {
+                take = await userCollection.findOne({ userID: student.userID });
+                console.log("Take= "+ take.displayName)
+                userToken = take.registrationToken;
+                console.log("userToken: "+ userToken);
+            } catch (err) {
+                console.log("Failure to retrieve registration token from db, error : " + err)
+            }
 
             const message = {
                 notification: { 
-                    title: 'Private Message from ' + senderName, 
-                    body: "You've got a private message from " + senderName, 
+                    title: "You've got a group message from " + senderName, 
+                    body: "You've got a group message notification in the course " + groupID, 
                 },
-                token: resultstudent.registrationToken,
+                token: userToken,
             };
 
-
             admin.messaging().send(message)
-            .then((response) => {
-                if (response.failureCount > 0) {
-                    const failedTokens = [];
-                    response.responses.forEach((resp, idx) => {
-                        if (!resp.success) {
-                            failedTokens.push(registrationTokens[idx]);
-                        }
-                    });
-                    console.log('Private message notification failure, token: ' + failedTokens);
-                }
-                else{
-                    console.log('Successfully sent message notification to a user : ' + resultstudent.userID);
-                }
-            }); 
-       }
-       catch (err) {
-           console.log("Failure to send private message notification, the error: " + err)
-       }
-    },
-
-    groupMessageNotification : async (senderName, groupID) => {
-
-        try {
-
-            var take, userToken
-
-            let resultstudents = await dbCourse.collection(groupID).find({}).project({ userID: 1, displayName: 1, _id: 0 }).toArray(); 
-            
-             resultstudents.forEach(  async student => {
-                try{
-                    take = await userCollection.findOne({ userID: student.userID });
-                    console.log("Take= "+ take.displayName)
-                    userToken = take.registrationToken;
-                    console.log("userToken: "+ userToken);
-                }
-                catch (err) {
-                    console.log("Failure to retrieve registration token from db, error : " + err)
-                }
-                const message = {
-                    notification: { 
-                        title: "You've got a group message from " + senderName, 
-                        body: "You've got a group message notification in the course " + groupID, 
-                    },
-                    token: userToken,
-                };
-
-                admin.messaging().send(message)
                 .then((response) => {
                     if (response.failureCount > 0) {
                         const failedTokens = [];
@@ -176,22 +146,12 @@ module.exports = {
                             }
                         });
                         console.log('Group message notification failure, token: ' + failedTokens);
-                    }
-                    else{
+                    } else {
                         console.log('Successfully sent the group message notification to a user : ' + resultstudents.userID);
                     }
                 }); 
-
-
-
-            });
-
-       }
-       catch (err) {
-           console.log("Failure to send private message notification, the error: " + err)
-       }        
+        });      
     }
-
 
     // testMessageSyntax : (thetoken) => {
         
