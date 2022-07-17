228f139bfc9993699315e388c552605966648c50
i951 <rebeccatslee@gmail.com>
2022-07-17 00:11:31 -0700

    fix ClickButtonTest issues
    unused imports and class naming conventions

--- frontend/app/src/androidTest/java/com/example/findyourpeers/ClickButtonTest.java
+++ frontend/app/src/androidTest/java/com/example/findyourpeers/ClickButtonTest.java
@@ -1,34 +1,25 @@
 package com.example.findyourpeers;
 
-import static android.app.PendingIntent.getActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertEquals;
 
-import android.content.Context;
 import android.view.View;
-
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class clickButtonTest {
+public class ClickButtonTest {
     @Rule
     public ActivityScenarioRule<LoginPage> activityRule =
             new ActivityScenarioRule<>(LoginPage.class);
@@ -46,7 +37,7 @@ public class clickButtonTest {
     }
 
     @Test
-    public void useAppContext() {
+    public void loginButtonTest() {
         onView(withId(R.id.button_login))
                 .perform(click());
 
