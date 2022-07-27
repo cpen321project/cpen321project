963b29c8a3e8c9975dfdf71de86dc1fdab0cfaf1
i951 <rebeccatslee@gmail.com>
2022-07-26 23:24:08 -0700

    fix codacy issues in Urls, pushNotifiService #3

--- frontend/app/src/main/java/com/example/findyourpeers/PushNotificationService.java
+++ frontend/app/src/main/java/com/example/findyourpeers/PushNotificationService.java
@@ -10,14 +10,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.findyourpeers.CreateProfile;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
 
 public class PushNotificationService extends FirebaseMessagingService {
 
